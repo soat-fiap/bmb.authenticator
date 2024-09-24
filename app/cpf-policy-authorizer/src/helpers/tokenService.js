800ca51a1c8a4d654621e5462d8fc835264726ab
italo <italo.pessoa@hotmail.com>
2024-09-24 09:34:35 -0300

    add logs

--- app/cpf-policy-authorizer/src/helpers/tokenService.js
+++ app/cpf-policy-authorizer/src/helpers/tokenService.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export function generateAccessToken(payload) {
+  console.log("generating access token: ", payload);
   return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: parseInt(process.env.ACCESS_TOKEN_EXP),
     issuer: process.env.ACCESS_TOKEN_ISSUER,
